27a6ae33a3db4eb5694c85d991dd8c587213fceb
artginzburg <art.ginzburg@ya.ru>
2021-10-24 06:41:51 +0300

    add test time measurement, fix broken destructuring

--- test.js
+++ test.js
@@ -2,4 +2,12 @@ const getUserDownloads = require('./index.js');
 
 require('dotenv').config();
 
-getUserDownloads(({ USER = 'artginzburg', PERSONAL_ACCESS_TOKEN } = process.env)).then(console.log);
+const { USER = 'artginzburg', PERSONAL_ACCESS_TOKEN } = process.env;
+
+const timeLabel = 'github-user-downloads';
+
+console.time(timeLabel);
+getUserDownloads(USER, PERSONAL_ACCESS_TOKEN).then((data) => {
+  console.log(data);
+  console.timeEnd(timeLabel);
+});
